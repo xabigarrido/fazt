7a50bcb806d21afc14b842b8200cbdfa62e33e2b
xabigarrido <xabigarrido@gmail.com>
2025-02-01 08:23:47 +0100

    euu

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,10 @@
 import axios from "./axios";
 
 export const registerRequest = async (user) =>
-  axios.post(`/auth/register`, user);
+  axios.post(`/auth/register`, user, { withCredentials: true });
 
-export const loginRequest = async (user) => axios.post(`/auth/login`, user);
+export const loginRequest = async (user) =>
+  axios.post(`/auth/login`, user, { withCredentials: true });
 
-export const verifyTokenRequest = async () => axios.get(`/auth/verify`);
+export const verifyTokenRequest = async () =>
+  axios.get(`/auth/verify`, { withCredentials: true });
